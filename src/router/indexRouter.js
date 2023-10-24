74c306a26adbc09969a5a64d7254caeab61ffe2c
Walttinho <walter.netto@live.com>
2023-10-24 19:54:29 -0300

    feat: add swagger

--- src/router/indexRouter.js
+++ src/router/indexRouter.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const routerAlunos = require('./alunosRouter')
+const swaggerRouter = require('./swaggerRoute')
+
 
 
 router.use('/alunos', routerAlunos)
-
+router.use("/doc", swaggerRouter)
 
 module.exports = router
