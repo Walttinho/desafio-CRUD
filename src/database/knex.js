542ed93d780b9812a3aba31c5546e4dfc221a004
Walttinho <walter.netto@live.com>
2023-10-24 17:43:32 -0300

    fix: correção conexão banco de dados

--- src/database/knex.js
+++ src/database/knex.js
@@ -1,18 +1,20 @@
-const knex = require("knex")({
-  client: "pg",
+ const knex = require("knex")({
+  client: 'pg',
   connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER||'postgres',
+    password: process.env.DB_PASSWORD||'postgres',
+    database: process.env.DB_NAME || "escola",
+    port: process.env.DB_PORT || 5432,
     ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
   },
-});
+}); 
+
+
 
 const conexao = async () => {
   await knex
-    .raw("SELECT * from alunos")
+    .raw("SELECT 1")
     .then(() => {
       console.log(`Conectado ao bando de dados ${process.env.DB_NAME}`);
     })
@@ -20,5 +22,6 @@ const conexao = async () => {
       console.error("Erro ao conectar ao banco de dados:", erro);
     });
 };
+conexao()
 
-module.exports = {knex, conexao};
+module.exports = knex
